3f70388cc74191e768ffbbfe872e12be66f1313d
CX <19302010084@fudan.edu.cn>
2020-06-13 09:46:39 +0800

    lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'root');
+define('DBPASS', 'Ycx1128418');
+define('DBCONNSTRING','mysql:host=localhost;dbname=Login;');
+?>
